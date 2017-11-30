1c455600c0a24bc90784ea2a05e05233712263f0
Stephen Shaw <shshaw@gmail.com>
2017-11-30 16:33:55 -0600

    Electron Vue prototype

---
+++ src/renderer/components/FloatingEmoji.vue
@@ -0,0 +1,76 @@
+<template>
+<span class="floating-emoji" :style="{ left: randomLeft(), animationDelay: Math.random() * -6 + 's' }">
+  <span class="floating-emoji__scale">
+    <span class="floating-emoji__wobble">
+    <slot>😆</slot>
+    </span>
+  </span>
+</span>
+
+</template>
+
+<script>
+
+  export default {
+    name: '',
+    data: ()=>({
+
+    }),
+
+    methods: {
+      randomLeft(){
+        return (window.innerWidth * 0.5)
+          + (
+            window.innerWidth * 0.25
+             * Math.random()
+             * (Math.random() < 0.5 ? -1 : 1 )
+            ) + 'px';
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+.floating-emoji {
+  display: block;
+  position: absolute;
+  bottom: 0em;
+  font-size: 20vw;
+  overflow: visibile;
+  width: 2em;
+  height: 2em;
+
+  animation: float 6s cubic-bezier(.69,.2,.93,.45) infinite;
+}
+
+@keyframes float {
+  to { transform: translateY(-100vh); }
+}
+
+
+.floating-emoji__scale {
+  display: block;
+  animation: inherit;
+  animation-name: scale;
+  animation-timing-function: linear;
+}
+
+@keyframes scale {
+  0%, 90%, 100% { opacity: 0; transform: scale(0.5); }
+  20% { opacity: 1; transform: scale(1); }
+}
+
+.floating-emoji__wobble {
+  display: block;
+  animation: wobble 1.5s ease-in-out infinite;
+  animation-delay: inherit;
+}
+
+@keyframes wobble {
+  0%, 100% { transform: translateX(-10%) translateX(-50%); }
+  50% { transform: translateX(10%) translateX(-50%); }
+}
+
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="wrapper">
+
+    <floating-emoji></floating-emoji>
+    <floating-emoji>👏</floating-emoji>
+    <floating-emoji>👏</floating-emoji>
+    <floating-emoji>👏</floating-emoji>
+    <floating-emoji>👍</floating-emoji>
+    <floating-emoji>👍</floating-emoji>
+    <floating-emoji>💖</floating-emoji>
+    <floating-emoji>💖</floating-emoji>
+    <floating-emoji>💖</floating-emoji>
+    <floating-emoji>💖</floating-emoji>
+
+    <div class="server-info">
+
+      <span class="server-info__live" v-if="server && server.url">
+        Cast your <strong>Emote Vote</strong> at <strong v-text="server.url"></strong>
+      </span>
+
+      <span class="server-info__status" v-else v-text="server.status"></span>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+  import FloatingEmoji from './FloatingEmoji';
+
+  export default {
+    name: 'landing-page',
+    components: {
+      SystemInformation,
+      FloatingEmoji,
+    },
+
+    data: ()=>({
+      server: {
+        url: 'http://192.168.0.23:1337/'
+      }
+    }),
+
+    mounted(){
+      // INITIALIZE SERVER
+    }
+  }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  user-select: none;
+  pointer-events: none;
+  text-shadow: none;
+}
+
+html, body {
+  width: 100%;
+  overflow: hidden;
+  background: transparent;
+}
+
+html { height: 100%; }
+body { min-height: 100%; }
+
+
+.server-info {
+  display: block;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  font-family: system, sans-serif;
+  text-align: center;
+  padding: 0.5em;
+  background:rgba(255,255,255,0.6);
+  text-shadow: 0 0 5px #FFF;
+  border-radius: 10px;
+  -webkit-app-region: drag;
+  cursor: move;
+  pointer-events: auto;
+  z-index: 999;
+}
+
+.server-info:before {
+    content: '';
+    display: block;
+    position: fixed;
+    top: 0; right: 0; bottom: 0; left: 0;
+    margin: auto;
+    border: dashed 5px transparent;
+    pointer-events: none;
+}
+.server-info:hover::before { border-color: #000; }
+
+</style>
